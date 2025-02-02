f06ab92b3d276804f3f981acf8647d7f61d458af
bmuccini <muccini.bianca@gmail.com>
2025-02-02 09:25:27 +0000

    Ajout stop_simulation.sh pour arreter la simulation

---
+++ stop_simulation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "🛑 Arrêt de la simulation..."
+
+# Trouver et tuer tous les processus Python liés à la simulation
+pkill -f normal_trafic_gen.py
+pkill -f priority_trafic_gen.py
+pkill -f lights.py
+pkill -f coordinator.py
+
+echo "✅ Tous les processus ont été arrêtés."
+
+
+
+# Commandes à écrire dans le terminal
+
+# chmod +x stop_simulation.sh   # Donne les permissions d’exécution
+# ./stop_simulation.sh   # Lance le script pour arrêter tout
