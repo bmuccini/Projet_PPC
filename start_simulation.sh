49c8d2cbf6c6c6c67ba9a46ac3e4b9b369ef69aa
bmuccini <muccini.bianca@gmail.com>
2025-02-02 09:01:57 +0000

    Ajout start_simulation.sh pour lancer la simulation

---
+++ start_simulation.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "Démarrage de la simulation du carrefour..."
+
+# Exécuter les processus en arrière-plan (& permet de ne pas bloquer le terminal)
+python3 normal_trafic_gen.py &
+NORMAL_PID=$!
+
+python3 priority_trafic_gen.py &
+PRIORITY_PID=$!
+
+python3 lights.py &
+LIGHTS_PID=$!
+
+python3 coordinator.py &
+COORDINATOR_PID=$!
+
+echo "Tous les processus sont lancés !"
+
+# Attendre que tous les processus se terminent
+wait $NORMAL_PID
+wait $PRIORITY_PID
+wait $LIGHTS_PID
+wait $COORDINATOR_PID
+
+echo "Simulation terminée."
+
+
+
+# Commander à écrire dans le terminal
+
+# chmod +x start_simulation.sh   # Donne les permissions d’exécution
+# ./start_simulation.sh   # Lance le script
