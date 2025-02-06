84e4ce0fdcb5f2d6dc76f06b13d2800b0b35b1fb
bmuccini <muccini.bianca@gmail.com>
2025-02-06 00:00:59 +0000

    Ajout descriptions normal_trafic_gen.py

--- normal_trafic_gen.py
+++ normal_trafic_gen.py
@@ -14,7 +14,7 @@ def creation_files_messages():
     """Crée les files de messages en utilisant le mécanisme de communication inter-processus (IPC) de System V."""
     try:
         queue_nord = sysv_ipc.MessageQueue(KEY_NORD, sysv_ipc.IPC_CREAT)  # sysv_ipc.IPC_CREAT : flag qui indique que la file de messages doit être créée si elle n'existe pas déjà. Sinon qu'elle est simplement ouverte
-        queue_sud = sysv_ipc.MessageQueue(KEY_SUD, sysv_ipc.IPC_CREAT)
+        queue_sud = sysv_ipc.MessageQueue(KEY_SUD, sysv_ipc.IPC_CREAT)  # Crée et ouvre la file de messages sud
         queue_est = sysv_ipc.MessageQueue(KEY_EST, sysv_ipc.IPC_CREAT)
         queue_ouest = sysv_ipc.MessageQueue(KEY_OUEST, sysv_ipc.IPC_CREAT)
         return queue_nord, queue_sud, queue_est, queue_ouest
@@ -35,7 +35,7 @@ def generation_trafic_normal(queue_nord, queue_sud, queue_est, queue_ouest):
         message = pickle.dumps(vehicule)  #  module pickle sérialiser vehicule en une chaîne de bytes (format qui peut être transmis)
 
         if depart == "N":
-            queue_nord.send(message)  # Envoie un message dans la file de messages
+            queue_nord.send(message)  # Envoie un message dans la file de messages nord
             print(f"Véhicule Nord ajouté : {vehicule}")
         elif depart == "S":
             queue_sud.send(message)
@@ -50,5 +50,9 @@ def generation_trafic_normal(queue_nord, queue_sud, queue_est, queue_ouest):
         time.sleep(2)
 
 if __name__ == "__main__":
+    """
+    Point d'entrée du programme.
+    Crée les files de messages et commence la génération du trafic.
+    """
     queue_nord, queue_sud, queue_est, queue_ouest = creation_files_messages()
     generation_trafic_normal(queue_nord, queue_sud, queue_est, queue_ouest)
